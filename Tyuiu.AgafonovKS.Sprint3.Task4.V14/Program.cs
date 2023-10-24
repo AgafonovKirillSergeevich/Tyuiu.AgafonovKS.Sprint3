f3caa4cc6769e987af1f5c8a83e59d51449aa441
Кирилл Агафонов <Kirya_agafonov_2002@mail.ru>
2023-10-24 19:11:08 +0500

    Task4

---
+++ Tyuiu.AgafonovKS.Sprint3.Task4.V14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.AgafonovKS.Sprint3.Task4.V14.Lib;
+
+namespace Tyuiu.AgafonovKS.Sprint3.Task4.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Агафонов К. С. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *"); 
+            Console.WriteLine("* Вариант 14                                                              *");
+            Console.WriteLine("* Выполнил: Агафонов Кирилл Сергеевич | ИИПб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5,                        *");
+            Console.WriteLine("*  вычислить значение функции y=cos(x)/sin(x)                             *");
+            Console.WriteLine("* При х = 0 прервать цикл. Полученные значения суммировать.               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага: " + startValue);
+            Console.WriteLine("Конец шага: " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда: " + ds.Calculate(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
