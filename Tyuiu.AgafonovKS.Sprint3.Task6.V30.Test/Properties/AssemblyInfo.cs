f99b1ed1c99bb190673d1699b95204419b1ad0a3
Кирилл Агафонов <Kirya_agafonov_2002@mail.ru>
2023-10-25 13:31:47 +0500

    Task6

---
+++ Tyuiu.AgafonovKS.Sprint3.Task6.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AgafonovKS.Sprint3.Task6.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AgafonovKS.Sprint3.Task6.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5daef6e-1b66-4d37-a646-3e62bc800866")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.AgafonovKS.Sprint3.Task6.V30/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.AgafonovKS.Sprint3.Task6.V30.Lib;
+
+namespace Tyuiu.AgafonovKS.Sprint3.Task6.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #6 | Выполнил: Агафонов К. С. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #6                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант 30                                                              *");
+            Console.WriteLine("* Выполнил: Агафонов Кирилл Сергеевич | ИИПб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine(" Напишите программу, которая ищет среди целых чисел,                       ");
+            Console.WriteLine(" принадлежащих числовому отрезку [11, 17] количество всех делителей.       ");           
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 11;
+            int stopValue = 17;
+
+            Console.WriteLine("Начало отрезка: " + startValue);
+            Console.WriteLine("Конец отрезка: " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество делителей: " + ds.GetSumTheDivisors(startValue, startValue));
+            Console.ReadKey();
+        }
+    }
+}
